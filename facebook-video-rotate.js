255ea109060069f45fe0e9f529326564214ba1ef
gholk <85619andy@gmail.com>
2019-05-27 11:43:44 +0800

    scan nagative rotate ratio

--- facebook-video-rotate.js
+++ facebook-video-rotate.js
@@ -2,6 +2,7 @@ document.querySelectorAll('div[role=presentation]')
     .forEach(present => {
         const container = present.parentNode
         if (container.querySelector('video')) {
+            console.log('get video', container)
             const ratioOrigin = getRotateRatio(container.style.transform)
             const ratioNext = ratioOrigin - 0.25
             container.style.transform = `rotate(${ratioNext}turn)`
@@ -9,7 +10,7 @@ document.querySelectorAll('div[role=presentation]')
     })
 
 function getRotateRatio(cssString) {
-    const scan = cssString.match(/rotate\((\d+(\.\d+)?)turn\)/)
+    const scan = cssString.match(/rotate\((-?\d+(\.\d+)?)turn\)/)
     if (scan) {
         const ratio = Number(scan[1])
         return ratio
