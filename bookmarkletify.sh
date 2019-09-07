8455c207672eae57ad88462e67a99c96fd53dbe6
gholk <85619andy@gmail.com>
2019-09-07 20:33:05 +0800

    use uglify js compression

--- bookmarkletify.sh
+++ bookmarkletify.sh
@@ -4,7 +4,7 @@ letify() {
     local indent_js
     indent_js="$1"
     echo -n 'void function () {'
-    uglifyjs $indent_js | tr -d '\n'
+    uglifyjs -c <$indent_js | tr -d '\n'
     echo '}()'
 }
 
