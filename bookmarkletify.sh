7964156602f517763f42a95637d6eb12944643d1
gholk <85619andy@gmail.com>
2019-10-06 22:49:36 +0800

    add to-url command and help

--- bookmarkletify.sh
+++ bookmarkletify.sh
@@ -8,5 +8,34 @@ letify() {
     echo '}()'
 }
 
-indent_js="$1"
-letify $indent_js
+encode_url() {
+    echo -n javascript:
+    node -p 'encodeURIComponent(process.argv[1])' -- "$(cat)"
+}
+
+print_help() {
+    cat <<HELP
+usage
+        # generate minified javascript wraped in void function(){ ... }()
+        $0 script.js
+
+        # generate percentage encoded javascript url
+        $0 to-url script.js
+HELP
+}
+
+if [ $# -eq 1 ]
+then
+    letify "$1"
+elif [ "$1" = to-url ]
+then
+    shift
+    letify "$1" | encode_url
+else
+    echo -n unknown option:
+    printf ' "%s"' "$@"
+    printf "\n"
+    print_help
+    exit 1
+fi
+
