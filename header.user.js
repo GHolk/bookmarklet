9cece9ac423262a210fa3522406a830f00aa8394
gholk <85619andy@gmail.com>
2022-06-07 00:26:54 +0800

    some useful function

--- header.user.js
+++ header.user.js
@@ -7,3 +7,29 @@
 // @license GPLv3
 // @grant none
 // ==/UserScript==
+
+class GholkLib {
+    sleep(second) {
+        return new Promise(wake => setTimeout(wake, second*1000))
+    }
+    $(q, root = document) {
+        return root.querySelector(q)
+    }
+    $$(q, root = document) {
+        return root.querySelectorAll(q)
+    }
+    create(name, parent) {
+        const node = document.createElement(name)
+        if (parent) parent.appendChild(node)
+        return node
+    }
+    pipe(value, list) {
+        return list.reduce((v, f) => f(v), value)
+    }
+    bindr(object, thing) {
+        let f
+        if (typeof thing == 'string') f = object[thing]
+        else f = thing
+        return f.bind(object)
+    }
+}
